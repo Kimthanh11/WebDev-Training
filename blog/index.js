ca0df02174972da49c6de3b7ab06bc294f8c6a85
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-08 21:22:52 +0700

    Update morgan, handlebars

--- blog/index.js
+++ blog/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(morgan("combined"));
 app.get("/tin-tuc", (req, res) => {
-  res.send("Hello World!");
+  res.send(`<h1>Hello World!</h1>`);
 });
 
 app.listen(port, () => {
